6b35de4d592fff4e7bebae5827891b8f1ae419a7
lucianatux <lucianatux@yahoo.com.ar>
2024-02-19 16:47:57 -0300

    creacion de token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,45 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const register = async ( req, res) => {
     const {username, email, password} = req.body;
 
     try {
         //res.send("registering");
-        const newUser = new User({username, email, password});
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            username, 
+            email, 
+            password: passwordHash,
+        });
         const userSaved = await newUser.save();
-        res.json(userSaved);
+
+        jwt.sign({
+            id: userSaved._id,
+        }, 
+        "secret123",
+        {
+            expiresIn: "1d",
+        },
+        (err, token) => {
+            if (err) console.log(err);
+            res.cookie("token", token);
+            res.json({
+                messagge: "User created successfully",
+            });
+        },
+        );
+
+        // res.json({
+        //     id: userSaved._id,
+        //     username: userSaved.username,
+        //     email: userSaved.email,
+        //     createdAt: userSaved.createdAt,
+        //     updatedAt: userSaved.updatedAt,
+        // });
         console.log(userSaved);
     } catch (error) {
         console.log(error);
