fb149a418db9481003fcfb1d1c0538b20bbeef6b
lucianatux <lucianatux@yahoo.com.ar>
2024-02-19 14:05:14 -0300

    user model

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     try {
         await mongoose.connect('mongodb://127.0.0.1/merndb');  
-        console.log('DB is connected');
+        console.log('>>>DB is connected');
     } catch (error) {
         console.log(error);
     }
