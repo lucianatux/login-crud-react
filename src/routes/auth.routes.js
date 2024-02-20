60b4ebe29d27ecfdb75547614af4bb7f3d230ab2
lucianatux <lucianatux@yahoo.com.ar>
2024-02-20 20:08:46 -0300

    zod

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,11 +6,13 @@ import {
   profile,
 } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
-router.post("/api/register", register);
-router.post("/api/login", login);
+router.post("/api/register", validateSchema(registerSchema), register);
+router.post("/api/login", validateSchema(loginSchema), login);
 router.post("/api/logout", logout);
 router.get("/api/profile", authRequired, profile);
 
