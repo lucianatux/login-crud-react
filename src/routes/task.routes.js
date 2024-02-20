8438ea8d404af53d41974f159006ddd7cb681d41
lucianatux <lucianatux@yahoo.com.ar>
2024-02-20 15:10:09 -0300

    task crud

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,13 @@
 import {Router} from 'express';
 import {authRequired} from '../middlewares/validateToken.js';
+import {getTask, getTasks, createTask, updateTask, deleteTask, } from '../controllers/task.controller.js'
 
 const router = Router();
 
-router.get('/api/tasks', authRequired, (req, res) => {
-    res.send('tasks')
-});
+router.get('/api/tasks', authRequired, getTasks);
+router.get('/api/tasks/:id', authRequired, getTask);
+router.post('/api/tasks', authRequired, createTask);
+router.delete('/api/tasks/:id', authRequired, deleteTask);
+router.put('/api/tasks/:id', authRequired, updateTask);
 
 export default router;
