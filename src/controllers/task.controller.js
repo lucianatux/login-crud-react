40912b037199a974b549a6e23c75471acc724280
lucianatux <lucianatux@yahoo.com.ar>
2024-02-20 18:01:52 -0300

    Update task.controller.js

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,7 +1,9 @@
 import Task from "../models/task.model.js";
 
 export const getTasks = async (req, res) => {
-  const tasks = await Task.find();
+  const tasks = await Task.find({
+    user: req.user.id,
+  }).populate('user');
   res.json(tasks);
 };
 
@@ -19,7 +21,7 @@ export const createTask = async (req, res) => {
 };
 
 export const getTask = async (req, res) => {
-  const task = await Task.findById(req.params.id);
+  const task = await Task.findById(req.params.id).populate('user');
   if (!task) return res.status(404).json({ message: "Task not found" });
   res.json(task);
 };
@@ -27,7 +29,8 @@ export const getTask = async (req, res) => {
 export const deleteTask = async (req, res) => {
   const task = await Task.findByIdAndDelete(req.params.id);
   if (!task) return res.status(404).json({ message: "Task not found" });
-  res.json(task);
+  //res.json(task);
+  return res.sendStatus(204);
 };
 
 export const updateTask = async (req, res) => {
