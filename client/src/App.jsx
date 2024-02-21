376ec83b35b5b95630262b925594f157d05b852b
lucianatux <lucianatux@yahoo.com.ar>
2024-02-21 17:41:00 -0300

    rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
   return(
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}/>
+        <Route path="/login" element={<h1>Login</h1>}/>
+        <Route path="/register" element={<h1>Register</h1>}/>
+        <Route path="/tasks" element={<h1>Tasks page</h1>}/>
+        <Route path="/add-task" element={<h1>New task</h1>}/>
+        <Route path="/task/:id" element={<h1>Update Task</h1>}/>
+        <Route path="/profile" element={<h1>Profile</h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
