b256f8f3c181213fbeac529c79d959aa5fa24a14
lucianatux <lucianatux@yahoo.com.ar>
2024-02-21 16:02:49 -0300

    listo validacion de datos

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,12 +1,14 @@
 import {Router} from 'express';
 import {authRequired} from '../middlewares/validateToken.js';
-import {getTask, getTasks, createTask, updateTask, deleteTask, } from '../controllers/task.controller.js'
+import {getTask, getTasks, createTask, updateTask, deleteTask, } from '../controllers/task.controller.js';
+import {validateSchema} from '../middlewares/validator.middleware.js';
+import { createTaskSchema} from '../schemas/task.schema.js';
 
 const router = Router();
 
 router.get('/api/tasks', authRequired, getTasks);
 router.get('/api/tasks/:id', authRequired, getTask);
-router.post('/api/tasks', authRequired, createTask);
+router.post('/api/tasks', authRequired, validateSchema(createTaskSchema), createTask);
 router.delete('/api/tasks/:id', authRequired, deleteTask);
 router.put('/api/tasks/:id', authRequired, updateTask);
 
