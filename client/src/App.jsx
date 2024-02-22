8cf353196438d3b6074e55a35732c87a0c71505a
lucianatux <lucianatux@yahoo.com.ar>
2024-02-22 00:25:59 -0300

    registro de usuario

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>}/>
-        <Route path="/login" element={<h1>Login</h1>}/>
-        <Route path="/register" element={<h1>Register</h1>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
         <Route path="/tasks" element={<h1>Tasks page</h1>}/>
         <Route path="/add-task" element={<h1>New task</h1>}/>
         <Route path="/task/:id" element={<h1>Update Task</h1>}/>
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,40 @@
+import { useForm } from "react-hook-form";
+import { registerRequest } from "../api/auth";
+
+function RegisterPage() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit(async (values) => {
+    const res = await registerRequest(values);
+    console.log(res);
+  });
+
+  return (
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      <h1>Register Page</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          {...register("username", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 m-1 rounded-md"
+          placeholder="Username"
+        />
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 m-1 rounded-md"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 m-1 rounded-md"
+          placeholder="Password"
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
